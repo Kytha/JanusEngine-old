7f50bb478a529f97cf41f375dc2428ad0ee598fe
Kyle Thatcher <33584092+Kytha@users.noreply.github.com>
2021-12-31 18:06:32 -0500

    Add GLFW as dependency

--- Janus/src/Application.cpp
+++ Janus/src/Application.cpp
@@ -1,7 +1,13 @@
 #include "Application.h"
 #include <iostream>
+
 namespace Janus {
     Application::Application() {
         std::cout << Janus::Radians(180);
+        return;
+    }
+
+    void Application::Run() {
+        return;
     }
 }
--- Janus/src/Application.h
+++ Janus/src/Application.h
@@ -1,9 +1,11 @@
 #include "Core.h"
+#include <GLFW/glfw3.h>
 
 namespace Janus {
 	class Application
 	{
 	public:
         Application();
+        void Run();
     };
 }
